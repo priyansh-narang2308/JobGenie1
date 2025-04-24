3ea1f9908e44a0e04b5526d58deedaf7dafde968
Adithya Menon R <adukottan@gmail.com>
2025-04-24 19:29:54 +0530

    Reverted change on API Key loading and added .env.example

--- backend-python/app.py
+++ backend-python/app.py
@@ -1,17 +1,26 @@
 import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import google.generativeai as genai
 from fastapi.middleware.cors import CORSMiddleware
 
+load_dotenv()
+
 from routes import career_guidance_routes
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+genai.configure(api_key=GEMINI_API_KEY)
 
 app.include_router(career_guidance_routes.router)
